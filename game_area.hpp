05bfb50f886efd925b74e6250a7c5c52a1bbff96
menagegit <menage237@hotmail.com>
2014-09-10 18:01:03 +0200

    Removing the tabs in game_area.hpp
    Signed-off-by: menagegit <menage237@hotmail.com>

--- game_area.hpp
+++ game_area.hpp
@@ -8,17 +8,17 @@
 class game_area_t
 {
 private:
-	int larg, haut;
-	float max_form_size;
-	std::list<geometric_form_t*>*** marked_forms;
+    int larg, haut;
+    float max_form_size;
+    std::list<geometric_form_t*>*** marked_forms;
 
 public:
-	game_area_t(int larg, int haut);
+    game_area_t(int larg, int haut);
 
     int get_hauteur() const { return haut; }
     int get_largeur() const { return larg; }
-	void mark_line(float sx, float sy, float ex, float ey, float lar, float angle);
-	bool is_marked(float pos, float lar, float radius);
+    void mark_line(float sx, float sy, float ex, float ey, float lar, float angle);
+    bool is_marked(float pos, float lar, float radius);
 };
 
 #endif /* POINT_JOUEUR_HPP */
